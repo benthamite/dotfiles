241b2b83b464ec236119a3cee1025ed44665c293
Pablo Stafforini <pablo@stafforini.com>
2023-09-06 16:12:47 +0300

    Add .dir-locals.el

---
+++ emacs/.dir-locals.el
@@ -0,0 +1,4 @@
+;;; Directory Local Variables            -*- no-byte-compile: t -*-
+;;; For more information see (info "(emacs) Directory Variables")
+
+((elisp-mode . ((flycheck-disabled-checkers . emacs-lisp-checkdoc))))

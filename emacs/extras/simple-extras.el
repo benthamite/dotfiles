ac741ffe6bd01480657ecbded84bb22731ac5577
Pablo Stafforini <pablo@stafforini.com>
2024-01-19 15:41:39 -0300

    simple-extras: add simple-extras-advice-remove-all

--- emacs/extras/simple-extras.el
+++ emacs/extras/simple-extras.el
@@ -617,5 +617,11 @@ If PT is non-nil, start at that position instead of `point'."
   (dolist (fun funs)
     (run-with-timer seconds nil (lambda () (advice-add fun :override #'ignore)))))
 
+;; emacs.stackexchange.com/a/24658/32089
+(defun simple-extras-advice-remove-all (sym)
+  "Remove all advices from symbol SYM."
+  (interactive "aFunction symbol: ")
+  (advice-mapc (lambda (advice _props) (advice-remove sym advice)) sym))
+
 (provide 'simple-extras)
 ;;; simple-extras.el ends here

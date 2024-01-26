bb0efc1c25f9b0c28edd237959c0a8b10add3daa
Pablo Stafforini <pablo@stafforini.com>
2024-01-26 16:04:56 -0300

    files-extras: add dispatcher

--- emacs/extras/files-extras.el
+++ emacs/extras/files-extras.el
@@ -509,5 +509,47 @@ current helpful buffer displays, then kill the buffer."
     (while (re-search-forward "\\(^\\s-*$\\)\n\\(\\(^\\s-*$\\)\n\\)+" nil t)
       (replace-match "\n"))))
 
+;;;;; Dispatcher
+
+(transient-define-prefix files-extras-dispatch ()
+  "Dispatcher for files."
+  ["Files"
+   ("." "inbox-mobile"    (lambda () (interactive) (find-file paths-file-inbox-mobile)))
+   ("," "inbox-desktop"   (lambda () (interactive) (find-file paths-file-inbox-desktop)))
+   ("a" "agenda"          org-extras-agenda-switch-to-agenda-current-day)
+   ("c" "config"          (lambda () (interactive) (find-file paths-file-config)))
+   ("d" "tlon-docs"       (lambda () (interactive) (find-file paths-file-tlon-docs)))
+   ("e" "extras"          (lambda () (interactive) (files-extras-open-extras-package)))
+   ("h" "tlon-ledger"     (lambda () (interactive) (find-file paths-file-tlon-ledger)))
+   ("i" "anki"            (lambda () (interactive) (find-file paths-file-anki)))
+   ("j" "ledger"          (lambda () (interactive) (find-file paths-file-ledger)))
+   ("k" "karabiner"       (lambda () (interactive) (find-file paths-file-karabiner)))
+   ("o" "notes"           (lambda () (interactive) (switch-to-buffer "*notes*")))
+   ("p" "packages"        files-extras-packages-dispatch)
+   ("q" "quotes"          (lambda () (interactive) (find-file paths-file-quotes)))
+   ("r" "calendar"        (lambda () (interactive) (find-file paths-file-calendar)))
+   ("s" "scratch"         (lambda () (interactive) (switch-to-buffer "*scratch*")))
+   ("v" "films"           (lambda () (interactive) (find-file paths-file-films)))
+   ("w" "work"            (lambda () (interactive) (find-file paths-file-work)))
+   ("z" "variables"       (lambda () (interactive) (find-file paths-file-variables)))
+   ])
+
+(transient-define-prefix files-extras-packages-dispatch ()
+  "Dispatcher for personal package files."
+  [["Tlön"
+    ("b" "tlon-babel"       (lambda () (interactive) (files-extras-open-elpaca-package "tlon-babel")))
+    ("c" "tlon-core"        (lambda () (interactive) (files-extras-open-elpaca-package "tlon-core")))
+    ("i" "tlon-init"        (lambda () (interactive) (files-extras-open-elpaca-package "tlon-init")))
+    ]
+   ["Personal"
+    ("a" "internet-archive" (lambda () (interactive )(files-extras-open-elpaca-package "internet-archive")))
+    ("l" "bib"              (lambda () (interactive )(files-extras-open-elpaca-package "bib")))
+    ("g" "glondendict-ng"   (lambda () (interactive )(files-extras-open-elpaca-package "goldendict-ng")))
+    ("o" "macos"            (lambda () (interactive )(files-extras-open-elpaca-package "macos")))
+    ("m" "mullvad"          (lambda () (interactive )(files-extras-open-elpaca-package "mullvad")))
+    ("s" "scihub"           (lambda () (interactive )(files-extras-open-elpaca-package "scihub")))
+    ("p" "pomodoro-centile" (lambda () (interactive ) (files-extras-open-elpaca-package "pomodoro-centile")))
+    ]])
+
 (provide 'files-extras)
 ;;; files-extras.el ends here

49a471c52ba8a0af28e3c90b2477d328089cde5b
Pablo Stafforini <pablo@stafforini.com>
2024-03-30 22:12:06 -0300

    simple-extras: move asciify functions from bibtex-extras

--- emacs/extras/simple-extras.el
+++ emacs/extras/simple-extras.el
@@ -667,12 +667,75 @@ language."
       (setq output (shell-command-to-string (format command "pbpaste" non-html))))
     output))
 
+;;;;; asciify
+
+;; Adapted from xahlee.info/emacs/emacs/emacs_zap_gremlins.html
+(defun simple-extras-asciify-text (&optional begin end)
+  "Remove accents in some letters. e.g. café → cafe.
+Change European language characters into equivalent ASCII ones.
+When called interactively, work on current line or text selection.
+
+Optionally, remove accents in region from BEGIN to END."
+  (interactive)
+  (let (($charMap
+         [
+          ["ß" "ss"]
+          ["á\\|à\\|â\\|ä\\|ā\\|ǎ\\|ã\\|å\\|ą\\|ă\\|ạ\\|ả\\|ả\\|ấ\\|ầ\\|ẩ\\|ẫ\\|ậ\\|ắ\\|ằ\\|ẳ\\|ặ" "a"]
+          ["æ" "ae"]
+          ["ç\\|č\\|ć" "c"]
+          ["é\\|è\\|ê\\|ë\\|ē\\|ě\\|ę\\|ẹ\\|ẻ\\|ẽ\\|ế\\|ề\\|ể\\|ễ\\|ệ" "e"]
+          ["í\\|ì\\|î\\|ï\\|ī\\|ǐ\\|ỉ\\|ị" "i"]
+          ["ñ\\|ň\\|ń" "n"]
+          ["ó\\|ò\\|ô\\|ö\\|õ\\|ǒ\\|ø\\|ō\\|ồ\\|ơ\\|ọ\\|ỏ\\|ố\\|ổ\\|ỗ\\|ộ\\|ớ\\|ờ\\|ở\\|ợ" "o"]
+          ["ú\\|ù\\|û\\|ü\\|ū\\|ũ\\|ư\\|ụ\\|ủ\\|ứ\\|ừ\\|ử\\|ữ\\|ự"     "u"]
+          ["ý\\|ÿ\\|ỳ\\|ỷ\\|ỹ"     "y"]
+          ["þ" "th"]
+          ["ď\\|ð\\|đ" "d"]
+          ["ĩ" "i"]
+          ["ľ\\|ĺ\\|ł" "l"]
+          ["ř\\|ŕ" "r"]
+          ["š\\|ś" "s"]
+          ["ť" "t"]
+          ["ž\\|ź\\|ż" "z"]
+	  ["­" ""]       ; soft hyphen
+          [" " " "]       ; thin space
+          ["–" "-"]       ; dash
+          ["—\\|一" "--"] ; em dash etc
+	  ["¿" ""]
+	  ["¡" ""]
+	  ["…" ""]
+          ])
+        ($p1 (if begin begin
+               (if (region-active-p)
+                   (region-beginning)
+                 (line-beginning-position))))
+        ($p2 (if end end
+               (if (region-active-p)
+                   (region-end)
+                 (line-end-position)))))
+    (let ((case-fold-search t))
+      (save-restriction
+        (narrow-to-region $p1 $p2)
+        (mapc
+         (lambda ($pair)
+           (goto-char (point-min))
+           (while (re-search-forward (elt $pair 0) (point-max) t)
+             (replace-match (elt $pair 1))))
+         $charMap)))))
+
+(defun simple-extras-asciify-string (string)
+  "Return a new STRING e.g. café → cafe."
+  (with-temp-buffer
+    (insert string)
+    (simple-extras-asciify-text (point-min) (point-max))
+    (buffer-string)))
+
 ;;;;; slugify
 
 ;;;###autoload
 (defun simple-extras-slugify (string)
   "Convert STRING into slug."
-  (bibtex-extras-asciify-string (prot-eww--sluggify string)))
+  (simple-extras-asciify-string (prot-eww--sluggify string)))
 
 ;;;###autoload
 (defun simple-extras-slugify-clipboard ()

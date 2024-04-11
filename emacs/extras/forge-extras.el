005e6008a1956d4f669dd1834d5d417f7ac37cb6
Pablo Stafforini <pablo@stafforini.com>
2024-04-11 07:24:48 -0300

    forge-extras: remove unused dispatcher

--- emacs/extras/forge-extras.el
+++ emacs/extras/forge-extras.el
@@ -91,57 +91,5 @@ problems."
   (unless (bound-and-true-p elfeed-extras-auto-update-in-process)
     (forge-pull-notifications)))
 
-;;;;; Menus
-
-(transient-define-prefix forge-extras-dispatch ()
-  "Dispatch a forge command."
-  [["Fetch"
-    ("f f" "all topics"             forge-pull)
-    ("f t" "one topic"              forge-pull-topic)
-    ("f n" "notifications"          forge-pull-notifications)
-    """Create"
-    ("c i" "issue"                  forge-create-issue)
-    ("c p" "pull-request"           forge-create-pullreq)
-    ("c u" "pull-request from issue" forge-create-pullreq-from-issue
-     :if (lambda () (forge-github-repository-p (forge-get-repository nil))))
-    ("c f" "fork or remote"        forge-fork)]
-   ["List"
-    ("l a" "awaiting review"        forge-list-requested-reviews)
-    ("l i" "issues"                 forge-list-issues)
-    ("l n" "notifications"          forge-list-notifications)
-    ("l p" "pull-requests"          forge-list-pullreqs)
-    ("l t" "topics"                 forge-list-topics)
-    ("l r" "repositories"           forge-list-repositories)
-    """Edit"
-    ("e t" "edit title"             forge-topic-set-title)
-    ("e s" "edit state"             forge-topic-state-menu)
-    ("e l" "edit labels"            forge-topic-set-labels)]
-   ["Browse"
-    ("b i" "issue"                  forge-browse-issue)
-    ("b p" "pull-request"           forge-browse-pullreq)
-    ("b r" "remote"                 forge-browse-remote)
-    ("b t" "topic"                  forge-browse-topic)
-    ("b I" "issues"                 forge-browse-issues)
-    ("b P" "pull-requests"          forge-browse-pullreqs)
-    """Visit"
-    ("v i" "issue"                  forge-visit-issue)
-    ("v p" "pull-request"           forge-visit-pullreq)
-    ("v t" "topic"                  forge-visit-topic)]
-   ["Authored"
-    ("u i" "authored issues"        forge-list-authored-issues)
-    ("u p" "authored pull-requests" forge-list-authored-pullreqs)
-    """Owned"
-    ("o i" "owned issues"           forge-list-owned-issues)
-    ("o p" "owned pull-requests"    forge-list-owned-pullreqs)]
-   ["Assigned"
-    ("i i" "assigned issues"        forge-list-assigned-issues)
-    ("i p" "assigned pull-requests" forge-list-assigned-pullreqs)
-    """Labeled"
-    ("d i" "labeled issues"         forge-list-labeled-issues)
-    ("d p" "labeled pull-requests"  forge-list-labeled-pullreqs)]
-   ["Misc"
-    ("s" "search topics"            forge-search)
-    (";" "Show/hide closed topics"  forge-toggle-closed-visibility)]])
-
 (provide 'forge-extras)
 ;;; forge-extras.el ends here

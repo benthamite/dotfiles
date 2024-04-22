20de312682ac93deba7c8edb1b2615dfb6da9d81
Pablo Stafforini <pablo@stafforini.com>
2024-04-21 22:38:02 -0300

    faces-extras: update code to handle setting of complex face attributes

--- emacs/extras/faces-extras.el
+++ emacs/extras/faces-extras.el
@@ -59,71 +59,47 @@ These attributes can be set with `faces-extras-set-custom-face-attributes'.")
 
 ;;;; Functions
 
-(defun faces-extras-set-face-attribute (attribute)
-  "Set a single face ATTRIBUTE."
-  (let ((face (car attribute))
-        (args (cdr attribute)))
-    (apply 'set-face-attribute face nil args)))
+;;;;; Face attributes
+
+(defun faces-extras-set-face-attribute (face attribute value)
+  "Set a single FACE ATTRIBUTE to VALUE, evaluating VALUE if it's a quoted expression."
+  ;; Check if value is a quoted expression and evaluate it.
+  (let ((evaluated-value (if (and (listp value) (eq (car value) 'quote))
+                             (eval value)
+                           value)))
+    (set-face-attribute face nil attribute evaluated-value)))
 
 (defun faces-extras-set-face-attributes (attributes)
   "Set a list of face ATTRIBUTES."
   (dolist (attribute attributes)
-    (faces-extras-set-face-attribute attribute)))
+    (let* ((face (car attribute))
+           (attr-vals (cdr attribute)))
+      (while attr-vals
+        (let ((attr (car attr-vals))
+              (val (cadr attr-vals)))
+          (faces-extras-set-face-attribute face attr val))
+        (setq attr-vals (cddr attr-vals))))))
 
 (defun faces-extras-set-and-store-face-attributes (attributes)
   "Set list of face ATTRIBUTES and store them.
-The attributes are stored in `faces-extras-custom-face-attributes'."
+The attributes are stored in `faces-extras-custom-face-attributes', with expressions evaluated."
   (dolist (attribute attributes)
-    (faces-extras-set-face-attribute attribute)
-    (add-to-list 'faces-extras-custom-face-attributes attribute)))
+    ;; Here we directly call `faces-extras-set-face-attributes' to maintain your original design pattern.
+    (faces-extras-set-face-attributes (list attribute))
+    ;; Store the attributes including evaluated values. This assumes attributes are passed with `quote' for evaluable values.
+    (let ((stored-attr (let* ((face (car attribute))
+                              (vals (cdr attribute))
+                              (evaluated-vals (mapcar (lambda (x) (if (listp x) (eval x) x)) vals)))
+                         (cons face evaluated-vals))))
+      (add-to-list 'faces-extras-custom-face-attributes stored-attr t))))
 
+;;;###autoload
 (defun faces-extras-set-custom-face-attributes ()
-  "Set custom face attributes."
+  "Set custom face attributes stored in `faces-extras-custom-face-attributes'."
   (interactive)
   (faces-extras-set-face-attributes faces-extras-custom-face-attributes))
 
-(defun faces-extras-load-custom-faces ()
-  "My custom faces, to be used in conjunction with theme."
-  (interactive)
-  (set-face-attribute 'default nil :family faces-extras-fixed-pitch-font :height faces-extras-fixed-pitch-size)
-  (set-face-attribute 'fixed-pitch nil :family faces-extras-fixed-pitch-font :height 1.1)
-  (set-face-attribute 'variable-pitch nil :family faces-extras-variable-pitch-font :height 1.4)
-  (set-face-attribute 'org-drawer nil :foreground "LightSkyBlue" :family faces-extras-fixed-pitch-font :height 0.8)
-  (set-face-attribute 'org-property-value nil :family faces-extras-fixed-pitch-font :height 0.8)
-  (set-face-attribute 'org-todo nil :family faces-extras-fixed-pitch-font :height 1.0)
-  (set-face-attribute 'org-archived nil :family faces-extras-fixed-pitch-font :height 0.9)
-  (set-face-attribute 'org-document-title nil :family faces-extras-fixed-pitch-font :height 1.0)
-  (set-face-attribute 'org-special-keyword nil :family faces-extras-fixed-pitch-font :height 0.8)
-  (set-face-attribute 'org-tag nil :family faces-extras-fixed-pitch-font :height 0.9)
-  (set-face-attribute 'org-code nil :family faces-extras-fixed-pitch-font :height 1.3)
-  (set-face-attribute 'org-level-1 nil :family faces-extras-fixed-pitch-font :height 0.9)
-  (set-face-attribute 'org-level-2 nil :family faces-extras-fixed-pitch-font :height 0.9)
-  (set-face-attribute 'org-level-3 nil :family faces-extras-fixed-pitch-font :height 0.9)
-  (set-face-attribute 'org-level-4 nil :family faces-extras-fixed-pitch-font :height 0.9)
-  (set-face-attribute 'org-level-5 nil :family faces-extras-fixed-pitch-font :height 0.9)
-  (set-face-attribute 'org-level-6 nil :family faces-extras-fixed-pitch-font :height 0.9)
-  (set-face-attribute 'org-level-7 nil :family faces-extras-fixed-pitch-font :height 0.9)
-  (set-face-attribute 'org-level-8 nil :family faces-extras-fixed-pitch-font :height 0.9)
-  (set-face-attribute 'org-date nil :family faces-extras-fixed-pitch-font :height 0.8)
-  (set-face-attribute 'org-quote nil :family faces-extras-variable-pitch-font :height 1.3)
-  (when (bound-and-true-p org-modern-mode)
-    (set-face-attribute 'org-modern-date-active nil :family faces-extras-fixed-pitch-font :height 0.8)
-    (set-face-attribute 'org-modern-date-inactive nil :family faces-extras-fixed-pitch-font :height 0.8)
-    (set-face-attribute 'org-modern-tag nil :family faces-extras-fixed-pitch-font :height 0.9)
-    (set-face-attribute 'org-modern-label nil :family faces-extras-fixed-pitch-font :height 0.8))
-  (set-face-attribute 'shr-text nil :height 0.65)
-  (when (bound-and-true-p corfu-mode)
-    (set-face-attribute 'corfu-default nil :family faces-extras-fixed-pitch-font :height 1))
-  (when (bound-and-true-p flycheck-mode)
-    (set-face-attribute 'flycheck-error nil :underline '(:color "#ff0000" :style wave))
-    (set-face-attribute 'flycheck-warning nil :underline '(:color "#0000ff" :style wave)))
-  (when (bound-and-true-p jinx-mode)
-    (set-face-attribute 'jinx-misspelled nil :underline '(:color "#008000" :style wave)))
-  (set-face-attribute 'window-divider nil :foreground (face-attribute 'mode-line-inactive :background))
-  (set-face-attribute 'tab-bar nil
-		      :background (face-background 'mode-line)
-		      ;; slightly increase the width of the tab-bar
-		      :box `(:line-width 6 :color ,(face-attribute 'mode-line :background) :style nil)))
+;;;;; Misc
 
 ;; github.com/arunkmv/.config/tree/main/emacs#tab-bar
 (defface faces-extras-display-time

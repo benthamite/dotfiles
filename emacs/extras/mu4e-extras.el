36cfec54201cbdca5beaeae56918645950df23f7
Pablo Stafforini <pablo@stafforini.com>
2024-08-05 11:05:46 -0300

    extras: make run-in-background optional

--- emacs/extras/mu4e-extras.el
+++ emacs/extras/mu4e-extras.el
@@ -132,7 +132,7 @@ function marks the saved copy as read."
 
 ;;;;; Commands
 
-(defun mu4e-extras-update-all-mail-and-index (run-in-background)
+(defun mu4e-extras-update-all-mail-and-index (&optional run-in-background)
   "Like `mu4e-update-mail-and-index', but include the \"all mail\" folder.
 If RUN-IN-BACKGROUND is non-nil (or called with prefix-argument), run in the
 background; otherwise, pop up a window."

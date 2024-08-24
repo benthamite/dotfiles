e5df8e72c261e06e63e9486e1ff24b6a89505a9e
Pablo Stafforini <pablo@stafforini.com>
2024-08-24 19:04:13 -0300

    org-noter-extras: quote string

--- emacs/extras/org-noter-extras.el
+++ emacs/extras/org-noter-extras.el
@@ -69,7 +69,7 @@ heading in a quote."
 	    (goto-char (point-max))
 	    (insert (concat quote-begin content quote-end))
 	    (goto-char (point-min))
-	    (re-search-forward (concat "\n" quote-end))
+	    (re-search-forward (concat "\n" (regexp-quote quote-end)))
 	    (replace-match quote-end)))
 	(org-next-visible-heading 1)
 	(org-fold-show-subtree)))))

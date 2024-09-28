483e653137947b845787dceec08e8d2ed871140f
Pablo Stafforini <pablo@stafforini.com>
2024-09-28 09:14:22 -0300

    browse-url-extras: autoload browse-url-extras-of-dired-file-externally

--- emacs/extras/browse-url-extras.el
+++ emacs/extras/browse-url-extras.el
@@ -69,6 +69,7 @@ respectively."
   (browse-url-extras-set-handler browse-url-extras-browse-url-default-file 'browse-url-default-browser)
   (browse-url-extras-set-handler browse-url-extras-browse-url-firefox-file 'browse-url-firefox))
 
+;;;###autoload
 (defun browse-url-extras-of-dired-file-externally ()
   "In Dired, open the file at point in an external browser."
   (interactive)

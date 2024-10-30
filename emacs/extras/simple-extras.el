147d926e9238d628d89f8da3c3274e33004e14a8
Pablo Stafforini <pablo@stafforini.com>
2024-10-30 13:24:53 -0300

    simple-extras: require org-extras in function

--- emacs/extras/simple-extras.el
+++ emacs/extras/simple-extras.el
@@ -481,6 +481,7 @@ number."
   (when (derived-mode-p 'org-mode 'org-agenda-mode 'org-msg-mode)
     (when (member 'org-tidy-mode org-mode-hook)
       (org-tidy-mode arg))
+    (require 'org-extras)
     (org-extras-inline-images arg)
     (when (and (not (derived-mode-p 'org-agenda-mode))
 	       (featurep 'org-modern))

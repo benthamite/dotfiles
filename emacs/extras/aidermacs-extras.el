aa0944cf9b5e6efa35c690e51f893cc5ef00ec63
Pablo Stafforini <pablo@stafforini.com>
2025-04-05 11:06:16 -0300

    aidermacs-extras: add aidermacs-extras.el

---
+++ emacs/extras/aidermacs-extras.el
@@ -0,0 +1,42 @@
+;;; aidermacs-extras.el --- Extensions for aidermacs -*- lexical-binding: t -*-
+
+;; Copyright (C) 2025
+
+;; Author: Pablo Stafforini
+;; URL: https://github.com/benthamite/dotfiles/tree/master/emacs/extras/aidermacs-extras.el
+;; Version: 0.1
+
+;; This file is NOT part of GNU Emacs.
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; Extensions for `aidermacs'.
+
+;;; Code:
+
+(require 'aidermacs)
+
+;;;; User options
+
+;;;; Main variables
+
+;;;; Functions
+
+
+
+(provide 'aidermacs-extras)
+;;; aidermacs-extras.el ends here
+

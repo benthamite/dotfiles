2c6acbd78dcade5f9e97fd7c17dc538634bde77a
Pablo Stafforini <pablo@stafforini.com>
2025-07-21 10:42:27 -0300

    Update lockfile

--- emacs/lockfile.el
+++ emacs/lockfile.el
@@ -1165,7 +1165,7 @@
  (init :source "elpaca-menu-lock-file" :recipe
        (:source nil :protocol https :inherit t :depth nil :host github :repo
 		"benthamite/init" :wait t :package "init" :ref
-		"b44552aed8d95f63e8c1998124b46996b24b8a61"))
+		"f830fe7faa9e71872fc48ae8cb2f67aaf3bfc043"))
  (isearch-extras :source "elpaca-menu-lock-file" :recipe
 		 (:source nil :protocol https :inherit t :depth nil :host github
 			  :repo "benthamite/dotfiles" :files

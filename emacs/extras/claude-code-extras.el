eea0395119d400e331d35f1c43be38e9a4d7ee76
Pablo Stafforini <pablo@stafforini.com>
2026-04-03 07:49:28 -0300

    claude-code-extras: use currentColor for icon to support light mode

--- emacs/extras/claude-code-extras.el
+++ emacs/extras/claude-code-extras.el
@@ -271,7 +271,7 @@ consumed by the Stop hook handler.")
 ;;;; Backend registration
 
 (defconst claude-code-extras-icon-svg
-  "<svg fill=\"none\" viewBox=\"0 0 24 24\" xmlns=\"http://www.w3.org/2000/svg\"><path clip-rule=\"evenodd\" d=\"M20.998 10.949H24v3.102h-3v3.028h-1.487V20H18v-2.921h-1.487V20H15v-2.921H9V20H7.488v-2.921H6V20H4.487v-2.921H3V14.05H0V10.95h3V5h17.998v5.949zM6 10.949h1.488V8.102H6v2.847zm10.51 0H18V8.102h-1.49v2.847z\" fill=\"#D97757\" fill-rule=\"evenodd\"/></svg>"
+  "<svg fill=\"currentColor\" viewBox=\"0 0 24 24\" xmlns=\"http://www.w3.org/2000/svg\"><path clip-rule=\"evenodd\" d=\"M20.998 10.949H24v3.102h-3v3.028h-1.487V20H18v-2.921h-1.487V20H15v-2.921H9V20H7.488v-2.921H6V20H4.487v-2.921H3V14.05H0V10.95h3V5h17.998v5.949zM6 10.949h1.488V8.102H6v2.847zm10.51 0H18V8.102h-1.49v2.847z\" fill=\"currentColor\" fill-rule=\"evenodd\"/></svg>"
   "SVG path data for the Claude Code mascot (Clawd pixel art).
 Source: lobehub/lobe-icons (MIT).")
 
